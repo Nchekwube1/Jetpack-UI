14dc7446d439e45d2b2280dc3026f70e7d5a3716
Unekwe Francis <unekwenchekwube@gmail.com>
2022-04-29 21:28:47 +0100

    changed icons to rounded

--- app/src/main/java/com/example/instaclone/bottomNav/BottomNavItem.kt
+++ app/src/main/java/com/example/instaclone/bottomNav/BottomNavItem.kt
@@ -1,16 +1,14 @@
 package com.example.instaclone.bottomNav
 
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.Icon
-import androidx.compose.material.icons.filled.*
+import androidx.compose.material.icons.rounded.*
 import androidx.compose.ui.graphics.vector.ImageVector
-import com.example.instaclone.R
 
 sealed class BottomNavItem (var title:String, var icon:ImageVector, var screen_route:String){
 
-            object Home : BottomNavItem("Home", icon = Icons.Default.Home,"Home")
-            object Market: BottomNavItem("Market",icon = Icons.Default.Store,"Market")
-            object Wallet: BottomNavItem("Wallet",icon = Icons.Filled.CreditCard,"Wallet")
-            object History: BottomNavItem("History",icon = Icons.Default.History,"History")
-            object Profile: BottomNavItem("Profile", icon = Icons.Default.Person,"Profile")
+            object Home : BottomNavItem("Home", icon = Icons.Rounded.Home,"Home")
+            object Market: BottomNavItem("Market",icon = Icons.Rounded.Store,"Market")
+            object Wallet: BottomNavItem("Wallet",icon = Icons.Rounded.CreditCard,"Wallet")
+            object History: BottomNavItem("History",icon = Icons.Rounded.History,"History")
+            object Profile: BottomNavItem("Profile", icon = Icons.Rounded.Person,"Profile")
 }
